9c503f18b87fba2ea156c16d126badfbde0340cc
SietseH <86694598+SietseH@users.noreply.github.com>
2021-08-25 07:17:06 +0300

    Update main.py
    Update

--- main.py
+++ main.py
@@ -1,25 +1,28 @@
+
 # Data
 
-RaceBikeTire = [[100],[[248, -11],[253, -9], [258, -8], [263, -7],
-[267, -4], [274, -2], [279, -1], [284, 0], [288, 2], [294, 3]]]
+RaceBikeTire = [[100], [[248, -11], [253, -9], [258, -8], [263, -7],
+                        [267, -4], [274, -2], [279, -1], [284, 0], [288, 2], [294, 3]]]
 
 NormalBikeTire = [[50], [[249, -9], [253, -8], [257, -6], [263, -5],
-[268, -3], [275, -2], [278, 0], [278, 0], [283, 0], [288, 1], [294, 2]]]
+                         [268, -3], [275, -2], [278, 0], [278, 0], [283, 0], [288, 1], [294, 2]]]
 
-ThicBikeTire = [[65],[[247, -9], [253, -8], [258, -7], [263, -6],
-[268, -4], [275, -1], [279, 0], [284, 0], [288, 0], [294, 2]]]
+ThicBikeTire = [[65], [[247, -9], [253, -8], [258, -7], [263, -6],
+                       [268, -4], [275, -1], [279, 0], [284, 0], [288, 0], [294, 2]]]
 
 PsiToPascal = 6894.76
+
 
 def psiPascal(name):
     output = name * PsiToPascal
     return output
 
+
 def initialPoint(data):
     psi = data[0][0]
     for point in data[1]:
         if point[1] == 0:
-           return point, psi, data
+            return point, psi, data
 
 
 def theoPressure(point):
@@ -28,17 +31,19 @@ def theoPressure(point):
     p1 = psiPascal(data[0][0])
     T1 = point[0][0]
     del data[0]
-    #print(data)
+    # print(data)
     for set in data[0]:
-
         T2 = set[0]
-        #print(T2, T1, p1)
-        p2 = (p1 * T2)/T1
+        # print(T2, T1, p1)
+        p2 = (p1 * T2) / T1
         dataset.append([T2, p2])
-    return dataset
-    
-
+    retstring = ""
+    for item in dataset:
+        retstring += str(item[0]) + " & " + str(int(round(item[1],-3)/1000)) + "\\\\\n"
+    return retstring
 
 print(theoPressure(initialPoint(RaceBikeTire)))
 print(theoPressure(initialPoint(NormalBikeTire)))
 print(theoPressure(initialPoint(ThicBikeTire)))
+
+
